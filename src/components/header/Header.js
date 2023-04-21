55d0bc14f76be772d7214eaccdfcad49fb314676
SebassPP <juanpapa@unisabana.edu.co>
2023-04-21 11:55:56 -0500

    ImplemetsBlog

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://verpelis.gratis/wp-content/uploads/2020/11/verpelis-logo.png" /></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
